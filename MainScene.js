71085e8aa6c02ab5e47814d18ea4918fe6328402
Anna <33619352+annty2@users.noreply.github.com>
2020-06-13 16:52:10 +0300

    Move the potions down
    Only one reel for now

--- MainScene.js
+++ MainScene.js
@@ -18,61 +18,86 @@ class MainScene extends Phaser.Scene {
     create() {
         
         //add container to display
-        var container = this.add.image(250,-20, 'slotContainer').setOrigin(0,0);
-        container.setScale(1.7);
+        this.slotContainer = this.add.image(250,-20, 'slotContainer').setOrigin(0,0);
+        this.slotContainer.setScale(1.7);
         
         
         //add title text 
-        var text= this.add.text(820, 83, "Slot House", {
-            font: "58px Arial",
-            color: "purple",
-            align: "center"
-        });
-        text.setAngle(-3);
+        this.text= this.add.text(820, 83, "Slot House", {
+                                font: "58px Arial",
+                                color: "purple",
+                                align: "center"});
+        this.text.setAngle(-3);
         
 
-        var spinButton = this.add.image(960,875, 'spinButton');
-        spinButton.setScale(1.7);
+        this.spinButton = this.add.image(960,875, 'spinButton');
+        this.spinButton.setScale(1.7);
 
-        spinButton.setInteractive();
+        this.spinButton.setInteractive();
         
-        spinButton.on('spinButtonClicked', this.onSpinClicked, this);
+        this.spinButton.on('spinButtonClicked', this.onSpinClicked, this);
 
         this.input.on('gameobjectup', function (pointer, gameObject) {
             gameObject.emit('spinButtonClicked', gameObject);
         }, this);;
 
         var potions = [];
+       // var reels = this.add.group();
+       var reels = this.add.container(0,0);
         
-        var container = this.add.container(400, 300);
+     
 
         for(let i=1; i<5; i++){
-            var sprite= this.add.sprite(95, -255+233*i, "potion"+i);
+            var sprite= this.add.sprite(495, 43+235*i, "potion"+i);
             sprite.setScale(1.7);
             potions.push(sprite);
-            container.add(sprite);
         }
+       
 
-
+        //reels.addMultiple(potions, this);
         //limit the potions to show only inside the container
-        var mask = this.add.graphics()
-	                    .setVisible(false)
-	                    .fillStyle(0xFFFFFF)             //white
-	                    .fillRect(377, 160, 1175, 705)   //(pos.x, pos.y, width, height)
-                        .createGeometryMask();
-        
-        //apply the mask on every object
-        potions.forEach(obj => obj.setMask(mask));
+        //and apply the mask on every potion
+        potions.forEach(potions => potions.setMask(this.add.graphics()
+                                                .setVisible(false)
+                                                .fillStyle(0xFFFFFF)             //white
+                                                .fillRect(377, 160, 1175, 705)   //(pos.x, pos.y, width, height)
+                                                .createGeometryMask()));
 
         console.log(potions);
+        console.log(potions[0].x);
+        console.log(potions[0].y);
 
-    }
+        //reels.addMultiple(potions);
+        reels.add(potions);
+
+
+
+        this.tweens.add({
+            targets: reels,
+            y: 1000 ,
+            duretion: 100,
+            repeat: -1
+        });
+
+}
 
     
 
-    onSpinClicked(spinButton) {
-        spinButton.setAlpha(0.5);
+    onSpinClicked() {
+        this.spinButton.setAlpha(0.5);
     }
+
+   /* update(){
+        for(let i=0; i<4; i++){
+            potions[i].y +=2;
+
+            if (potions[i].y > 950)
+            {
+                potions[i].y = -150;
+            }
+        }
+    
+    }*/
 
    
 
--- main.js
+++ main.js
@@ -1,18 +1,19 @@
+
+var game;
+
+window.addEventListener("load", () => {
 var config = {
-    type: Phaser.AUTO,
-    width: window.innerWidth,
-    height: window.innerHeight,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
-        }
-    },
-    scene: [ MainScene ]
+type: Phaser.AUTO,
+width: window.innerWidth,
+height: window.innerHeight,
+physics: {
+default: 'arcade',
+arcade: {
+gravity: { y: 100 }
+}
+},
+scene: [ MainScene ]
 };
 
-var game = new Phaser.Game(config);
-
-
-
-                 
+game = new Phaser.Game(config);
+});
